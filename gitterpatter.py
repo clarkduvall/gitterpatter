5c6da0825722cb1e6dda8ff89a6ea41d02d7da49
Clark DuVall <clark.duvall@gmail.com>
2014-02-13 22:57:18 -0800

    More oauth

--- gitterpatter.py
+++ gitterpatter.py
@@ -2,13 +2,24 @@ import requests
 
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__, static_url_path='', static_folder='static')
+
+ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+GITHUB_CLIENT_ID = os.environ['GITHUB_CLIENT_ID']
+GITHUB_CLIENT_SECRET = os.environ['GITHUB_CLIENT_SECRET']
 
 
 @app.route('/callback')
 def callback():
-    return request.args.get('code', '')
+    response = requests.post(ACCESS_TOKEN_URL, params={
+        'client_id': GITHUB_CLIENT_ID,
+        'client_secret': GITHUB_CLIENT_SECRET,
+        'code': request.args.get('code', '')
+    }, headers={'Accept': 'application/json'})
+
+    return response.json()
 
 
 @app.route('/', defaults={'path': ''})
