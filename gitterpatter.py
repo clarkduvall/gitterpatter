1fe59d5c4300f05609c3b27c5f4b893418ed1a85
Clark DuVall <clark.duvall@gmail.com>
2014-02-13 21:58:01 -0800

    Fix static

--- gitterpatter.py
+++ gitterpatter.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask
 
-app = Flask(__name__, static_url_path='static', static_folder='static')
+app = Flask(__name__, static_url_path='/static', static_folder='static')
 
 @app.route('/')
 def hello():
