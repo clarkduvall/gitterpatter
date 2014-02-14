e706263131397469a5fdaa9142a0ae1413a14988
Clark DuVall <clark.duvall@gmail.com>
2014-02-13 23:36:12 -0800

    More with session

--- gitterpatter.py
+++ gitterpatter.py
@@ -3,10 +3,15 @@ import os
 import requests
 
 from flask import Flask
+from flask import redirect
 from flask import render_template
 from flask import request
+from flask import session
+from flask import url_for
 
 app = Flask(__name__, static_url_path='', static_folder='static')
+app.debug = True
+app.secret_key = os.environ['FLASK_SECRET']
 
 ACCESS_TOKEN_URL = 'https://github.com/login/oauth/access_token'
 GITHUB_CLIENT_ID = os.environ['GITHUB_CLIENT_ID']
@@ -21,10 +26,13 @@ def callback():
         'code': request.args.get('code', '')
     }, headers={'Accept': 'application/json'})
 
-    return response.content
+    if response.ok:
+        session['access_token'] =  response.json()['access_token']
+
+    return redirect(url_for('.index'))
 
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    return render_template('index.html')
+    return render_template('index.html', token=session.get('access_token', ''))
