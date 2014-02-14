957feaeed38ed03eba4f579d22076df3601f2381
Clark DuVall <clark.duvall@gmail.com>
2014-02-13 22:03:09 -0800

    Change templates

--- gitterpatter.py
+++ gitterpatter.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
